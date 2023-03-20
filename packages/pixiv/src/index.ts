5a6603100c7a239d552ba7b5a6be035759fda199
Maiko Tan <maiko.tan.coding@gmail.com>
2023-03-20 14:57:44 +0800

    fix: pixiv get 400 errors

--- packages/pixiv/src/index.ts
+++ packages/pixiv/src/index.ts
@@ -1,19 +1,23 @@
-import { Context, Schema, trimSlash } from 'koishi'
+import { Context, Quester, Schema, SessionError, trimSlash } from 'koishi'
 import { ImageSource } from 'koishi-plugin-booru'
 
 export const name = 'koishi-plugin-booru-pixiv'
 
+export const using = ['booru']
+
 export interface Config extends ImageSource.Config {
   endpoint: string
   token?: string
+  pximgProxy?: string
 }
 
 export const Config = Schema.object({
-  label: Schema.string().default('lolicon').description('图源标签，可用于在指令中手动指定图源。'),
+  label: Schema.string().default('pixiv').description('图源标签，可用于在指令中手动指定图源。'),
   weight: Schema.number().default(1).description('图源权重。在多个符合标签的图源中，将按照各自的权重随机选择。'),
 
   endpoint: Schema.string().description('Pixiv 的 API Root').default('https://app-api.pixiv.net/'),
-  token: Schema.string().description('Pixiv 的 Refresh Token')
+  token: Schema.string().description('Pixiv 的 Refresh Token'),
+  pximgProxy: Schema.string().description('Pixiv 图片代理，用于解决图片无法访问的问题').default('https://i.pixiv.re/'),
 })
 
 const CLIENT_ID = 'MOBrBDS8blbauoSck0ZfDbtuzpyT'
@@ -26,7 +30,7 @@ export class PixivImageSource extends ImageSource<Config> {
   private userId?: string
   private accessToken?: string
   private refreshToken?: string
-  
+
   constructor(ctx: Context, config: Config) {
     super(ctx, config)
   }
@@ -35,7 +39,7 @@ export class PixivImageSource extends ImageSource<Config> {
     let url = this.config.token ? '/v1/illust/recommended' : '/v1/illust/recommended-nologin'
     const params = {
       content_type: 'illust',
-      include_ranking_label: true,
+      include_ranking_label: 'true',
       filter: 'for_ios',
     }
 
@@ -43,45 +47,66 @@ export class PixivImageSource extends ImageSource<Config> {
       await this._login()
     }
 
-    const data = await this.ctx.http.get(trimSlash(this.config.endpoint) + url, { params, headers: this._getHeaders() })
+    try {
+      const data = await this.ctx.http.get(trimSlash(this.config.endpoint) + url, {
+        params,
+        headers: this._getHeaders(),
+      })
 
-    return data.illusts.map((illust: any) => {
-      let url = ''
-      if (illust.page_count > 1) {
-        url = illust.meta_pages[0].image_urls.original
-      } else {
-        url = illust.meta_single_page.original_image_url
-      }
+      return data.illusts.map((illust: any) => {
+        let url = ''
+        if (illust.page_count > 1) {
+          url = illust.meta_pages[0].image_urls.original
+        } else {
+          url = illust.meta_single_page.original_image_url
+        }
 
-      return {
-        url,
-        title: illust.title,
-        pageUrl: `https://pixiv.net/i/${illust.id}`,
-        author: illust.user.name,
-        authorUrl: `https://pixiv.net/u/${illust.user.id}`,
-        desc: illust.caption,
-        tags: illust.tags.map((tag: any) => tag.name),
-        nsfw: illust.x_restrict >= 1,
-      }
-    })
+        if (this.config.pximgProxy) {
+          url = this.config.pximgProxy + url.replace(/^https?:\/\/i\.pximg\.net\//, '')
+        }
+
+        return {
+          url,
+          title: illust.title,
+          pageUrl: `https://pixiv.net/i/${illust.id}`,
+          author: illust.user.name,
+          authorUrl: `https://pixiv.net/u/${illust.user.id}`,
+          desc: illust.caption,
+          tags: illust.tags.map((tag: any) => tag.name),
+          nsfw: illust.x_restrict >= 1,
+        }
+      })
+    } catch (err) {
+      throw new SessionError('commands.booru.messages.no-response')
+    }
   }
 
   async _login() {
-    const endpoint = 'https://oauth.secure.pixiv.net' // OAuth Endpoint
+    const endpoint = 'https://oauth.secure.pixiv.net/' // OAuth Endpoint
     const url = trimSlash(endpoint) + '/auth/token'
 
     const data = {
-      'get_secure_url': 1,
-      'client_id': CLIENT_ID,
-      'client_secret': CLIENT_SECRET,
-      'refresh_token': this.config.token,
+      get_secure_url: true,
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      grant_type: 'refresh_token',
+      refresh_token: this.config.token,
     }
 
-    const resp = await this.ctx.http.axios(url, { method: 'POST', data, headers: this._getHeaders() })
+    const resp = await this.ctx.http.axios(url, {
+      method: 'POST',
+      data,
+      headers: {
+        ...this._getHeaders(),
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'host': 'oauth.secure.pixiv.net',
+      },
+      validateStatus: () => true,
+    })
 
     const SUCCESS_STATUS = [200, 301, 302]
     if (!SUCCESS_STATUS.includes(resp.status)) {
-      throw new Error('Login failed with status code ' + resp.status)
+      throw new Error('Login failed with status code ' + resp.status + JSON.stringify((resp as any).data))
     }
 
     this.userId = resp.data.user.id
