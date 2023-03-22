c9d06252fb0757c44883979e60e7eb3e84b01fec
Kabuda-czh <634469564@qq.com>
2023-03-22 09:59:56 +0800

    fix: change minimun value 0 to 1

--- packages/konachan/src/index.ts
+++ packages/konachan/src/index.ts
@@ -43,7 +43,7 @@ namespace KonachanImageSource {
 
   export const Config: Schema<Config> = Schema.object({
     label: Schema.string().default('konachan').description('图源标签，可用于在指令中手动指定图源。'),
-    weight: Schema.number().min(0).default(1).description('图源权重。在多个符合标签的图源中，将按照各自的权重随机选择。'),
+    weight: Schema.number().min(1).default(1).description('图源权重。在多个符合标签的图源中，将按照各自的权重随机选择。'),
     endpoint: Schema.union([
       Schema.const('https://konachan.com/').description('konachan.com (NSFW)'),
       Schema.const('https://konachan.net/').description('konachan.net (SFW)')
--- packages/yande/src/index.ts
+++ packages/yande/src/index.ts
@@ -44,7 +44,7 @@ namespace YandeImageSource {
 
   export const Config: Schema<Config> = Schema.object({
     label: Schema.string().default('yande').description('图源标签，可用于在指令中手动指定图源。'),
-    weight: Schema.number().min(0).default(1).description('图源权重。在多个符合标签的图源中，将按照各自的权重随机选择。'),
+    weight: Schema.number().min(1).default(1).description('图源权重。在多个符合标签的图源中，将按照各自的权重随机选择。'),
     endpoint: Schema.string().description('yande 的 URL。').default('https://yande.re'),
     login: Schema.string().description('yande 的用户名。').required(),
     apiKey: Schema.string().description('yande 的 API Key。').required(),
