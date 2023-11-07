7505e9d774947025c8eb619690a430f60939732c
Lipraty <sis1943@outlook.com>
2023-11-07 11:54:17 +0800

    refactor(core): using JSX

--- packages/core/src/index.tsx
+++ packages/core/src/index.tsx
@@ -240,6 +240,6 @@ export function apply(ctx: Context, config: Config) {
         }
       }
 
-      return output.length === 1 ? output[0] : `<message forward>${output.join('\n')}</message>`
+      return output.length === 1 ? output[0] : <message forward>{output.join('\n')}</message>
     })
 }
