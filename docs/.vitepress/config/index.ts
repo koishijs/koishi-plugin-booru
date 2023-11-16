f6074d345a6f8948c953aa9f35f52ab7e44fd4c9
Shigma <shigma10826@gmail.com>
2023-11-16 11:22:26 +0800

    build: add deploy script (#120)
    * build: add deploy script
    
    * build: move deploy to publish.yml

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   },
 
   themeConfig: {
-    indexName: 'booru',
+    indexName: 'koishi-booru',
     socialLinks: {
       github: 'https://github.com/koishijs/koishi-plugin-booru',
     },
