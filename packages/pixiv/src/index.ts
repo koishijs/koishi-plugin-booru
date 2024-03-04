koishijs/koishi-plugin-booru

commit 7548c54676f9cc7e0ceaae11169bfd418d0cc7b5

feat: support fetch API produced in Koishi v4.17.0 (#138)
* fix: encode data

* chore: use Object

* fix: use the best pratice one

---------

Co-authored-by: Maiko Tan <maiko.tan.coding@gmail.com>

packages/pixiv/src/index.ts
@@ -1,4 +1,4 @@
-import { Context, Schema, trimSlash } from 'koishi'
+import { Context, Quester, Schema, trimSlash } from 'koishi'
 import { ImageSource } from 'koishi-plugin-booru'
 import { PixivAppApi } from './types'
 
@@ -73,6 +73,9 @@ class PixivImageSource extends ImageSource<PixivImageSource.Config> {
           }
         })
     } catch (err) {
+      if (Quester.Error.is(err)) {
+        throw new Error('get pixiv image failed: ' + `${err.message} (${err.response?.status})`)
+      }
       return
     }
   }
@@ -81,17 +84,15 @@ class PixivImageSource extends ImageSource<PixivImageSource.Config> {
     const endpoint = 'https://oauth.secure.pixiv.net/' // OAuth Endpoint
     const url = trimSlash(endpoint) + '/auth/token'
 
-    const data = {
-      get_secure_url: true,
+    const data = new URLSearchParams({
+      get_secure_url: 'true',
       client_id: CLIENT_ID,
       client_secret: CLIENT_SECRET,
       grant_type: 'refresh_token',
       refresh_token: this.refreshToken,
-    }
+    })
 
-    const resp = await this.ctx.http.axios(url, {
-      method: 'POST',
-      data,
+    const resp = await this.ctx.http.post(url, data, {
       headers: {
         ...this._getHeaders(),
         'Content-Type': 'application/x-www-form-urlencoded',

commit 4dec19886ffaa0ab5f5788f0249fef5fce190002

fix: post login request broken in #138 (#139)
* fix: post login request broken im #138

* fix: redundant .data

* fix: add error reporting

packages/pixiv/src/index.ts
@@ -75,8 +75,9 @@ class PixivImageSource extends ImageSource<PixivImageSource.Config> {
     } catch (err) {
       if (Quester.Error.is(err)) {
         throw new Error('get pixiv image failed: ' + `${err.message} (${err.response?.status})`)
+      } else {
+        throw new Error('get pixiv image failed: ' + err)
       }
-      return
     }
   }
 
@@ -92,27 +93,30 @@ class PixivImageSource extends ImageSource<PixivImageSource.Config> {
       refresh_token: this.refreshToken,
     })
 
-    const resp = await this.ctx.http.post(url, data, {
-      headers: {
-        ...this._getHeaders(),
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'host': 'oauth.secure.pixiv.net',
-      },
-      validateStatus: () => true,
-    })
+    try {
+      const resp = await this.ctx.http.post(url, data, {
+        headers: {
+          ...this._getHeaders(),
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'host': 'oauth.secure.pixiv.net',
+        },
+        validateStatus: (status) => [200, 301, 302].includes(status),
+      })
 
-    const SUCCESS_STATUS = [200, 301, 302]
-    if (!SUCCESS_STATUS.includes(resp.status)) {
-      throw new Error('Login failed with status code ' + resp.status + JSON.stringify((resp as any).data))
+      this.userId = resp.user.id
+      this.accessToken = resp.access_token
+      this.refreshToken = resp.refresh_token
+      if (this.refreshTime) clearTimeout(this.refreshTime)
+      this.refreshTime = setTimeout(() => (this.accessToken = undefined), resp.expires_in * 1000)
+
+      return this.accessToken
+    } catch (err) {
+      if (Quester.Error.is(err)) {
+        throw new Error('Login failed with status code ' + err.response?.status + '\n' + JSON.stringify(err.response))
+      } else {
+        throw new Error('Login failed with unknown error: ' + err.message)
+      }
     }
-
-    this.userId = resp.data.user.id
-    this.accessToken = resp.data.access_token
-    this.refreshToken = resp.data.refresh_token
-    if (this.refreshTime) clearTimeout(this.refreshTime)
-    this.refreshTime = setTimeout(() => (this.accessToken = undefined), resp.data.expires_in * 1000)
-
-    return this.accessToken
   }
 
   _getHeaders() {