263e089d16e9844d1bea0af2fd1ebc10ca3df665
Maiko Tan <maiko.tan.coding@gmail.com>
2024-03-18 13:42:32 +0800

    feat: add sankaku api token support

---
+++ packages/sankaku/src/constants.ts
@@ -0,0 +1,10 @@
+export const BASE_URL = 'https://login.sankakucomplex.com'
+export const API_URL = 'https://capi-v2.sankakucomplex.com'
+
+export const LOGIN_URL = `${BASE_URL}/auth/token`
+export const POSTS_URL = `${API_URL}/posts`
+export const AI_POSTS_URL = `${API_URL}/ai_posts`
+export const TAGS_URL = `${API_URL}/tags`
+export const POOLS_URL = `${API_URL}/pools`
+export const USERS_URL = `${API_URL}/users`
+export const PROFILE_URL = `${USERS_URL}/me`
