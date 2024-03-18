8e49785e3d002bfe4f1202f4072c84ffe6f15dd0
Maiko Tan <maiko.tan.coding@gmail.com>
2024-03-18 14:08:31 +0800

    feat: add yande.re api token support

--- packages/yande/src/index.ts
+++ packages/yande/src/index.ts
@@ -1,6 +1,20 @@
+import { createHash } from 'node:crypto'
 import { Context, Schema, trimSlash } from 'koishi'
 import { ImageSource } from 'koishi-plugin-booru'
 import { Yande } from './types'
+
+/**
+ * Yande.re requires a password hash for authentication.
+ *
+ * @see https://yande.re/help/api
+ */
+function hashPassword(password: string) {
+  const salted = `choujin-steiner--${password}--`
+  // do a SHA1 hash of the salted password
+  const hash = createHash('sha1')
+  hash.update(salted)
+  return hash.digest('hex')
+}
 
 class YandeImageSource extends ImageSource<YandeImageSource.Config> {
   languages = ['en']
@@ -10,15 +24,30 @@ class YandeImageSource extends ImageSource<YandeImageSource.Config> {
     super(ctx, config)
   }
 
+  get keyPair() {
+    if (!this.config.keyPairs.length) return
+    const key = this.config.keyPairs[Math.floor(Math.random() * this.config.keyPairs.length)]
+    return {
+      login: key.login,
+      password_hash: hashPassword(key.password),
+    }
+  }
+
   async get(query: ImageSource.Query): Promise<ImageSource.Result[]> {
     // API docs: https://yande.re/help/api
     const params = {
-      tags: query.tags.join('+') + "+order:random",
-      limit: query.count
+      tags: [...query.tags, 'order:random'].join('+'),
+      limit: query.count,
     }
-    const url = trimSlash(this.config.endpoint) + '/post.json' + '?' + Object.entries(params).map(([key, value]) => `${key}=${value}`).join('&')
+    const url = trimSlash(this.config.endpoint) + '/post.json'
 
-    const data = await this.http.get<Yande.Response[]>(url)
+    const keyPair = this.keyPair
+    if (keyPair) {
+      params['login'] = keyPair.login
+      params['password_hash'] = keyPair.password_hash
+    }
+
+    const data = await this.http.get<Yande.Response[]>(url, { params })
 
     if (!Array.isArray(data)) {
       return
@@ -39,12 +68,22 @@ class YandeImageSource extends ImageSource<YandeImageSource.Config> {
 namespace YandeImageSource {
   export interface Config extends ImageSource.Config {
     endpoint: string
+    keyPairs: {
+      login: string
+      password: string
+    }[]
   }
 
   export const Config: Schema<Config> = Schema.intersect([
     ImageSource.createSchema({ label: 'yande' }),
     Schema.object({
       endpoint: Schema.string().description('Yande.re 的 URL。').default('https://yande.re'),
+      keyPairs: Schema.array(
+        Schema.object({
+          login: Schema.string().required().description('Yande.re 的用户名。'),
+          password: Schema.string().required().description('Yande.re 的密码。'),
+        }),
+      ).description('Yande.re 的登录凭据。'),
     }).description('搜索设置'),
   ])
 }
