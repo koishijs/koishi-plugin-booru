b0f0c7d46d58661d50e3e4383fa361de92c32248
Maiko Tan <maiko.tan.coding@gmail.com>
2024-08-14 11:12:14 +0800

    refa: migrate base schema text to i18n files

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -214,63 +214,47 @@ export const Config = Schema.intersect([
   Schema.intersect([
     Schema.union([
       Schema.object({
-        detectLanguage: Schema.boolean().default(false).description('自动检测输入语言并选择语言匹配的图源。'),
+        detectLanguage: Schema.boolean().default(false),
       }),
       Schema.object({
-        detectLanguage: Schema.const(true).description('自动检测输入语言并选择语言匹配的图源。'),
-        confidence: Schema.number().default(0.5).description('语言检测的置信度。'),
+        detectLanguage: Schema.const(true),
+        confidence: Schema.number().default(0.5),
       }),
     ]),
     Schema.object({
-      maxCount: Schema.number().default(10).description('每次搜索的最大数量。'),
-      nsfw: Schema.boolean().default(false).description('是否允许输出 NSFW 内容。'),
+      maxCount: Schema.number().default(10),
+      nsfw: Schema.boolean().default(false),
     }),
-  ]).description('搜索设置'),
+  ]),
   Schema.object({
-    output: Schema.union([
-      Schema.const(0).description('仅发送图片'),
-      Schema.const(1).description('发送图片和相关信息'),
-      Schema.const(2).description('发送图片、相关信息和链接'),
-      Schema.const(3).description('发送全部信息'),
-    ])
-      .description('输出方式。')
-      .default(1),
+    output: Schema.union([Schema.const(0), Schema.const(1), Schema.const(2), Schema.const(3)]).default(1),
     outputMethod: Schema.union([
-      Schema.const('one-by-one').description('逐条发送每张图片'),
-      Schema.const('merge-multiple').description('合并多条发送 (部分平台可能不支持)'),
-      Schema.const('forward-all').description('合并为子话题发送所有图片 (部分平台需求较高权限)'),
-      Schema.const('forward-multiple').description('仅当多于一张图片使用合并为子话题发送 (部分平台需求较高权限)'),
+      Schema.const('one-by-one'),
+      Schema.const('merge-multiple'),
+      Schema.const('forward-all'),
+      Schema.const('forward-multiple'),
     ])
       .experimental()
       .role('radio')
-      .default('merge-multiple')
-      .description('发送方式。'),
+      .default('merge-multiple'),
     preferSize: Schema.union([
-      Schema.const('original').description('原始尺寸'),
-      Schema.const('large').description('较大尺寸 (通常为约 1200px)'),
-      Schema.const('medium').description('中等尺寸 (通常为约 600px)'),
-      Schema.const('small').description('较小尺寸 (通常为约 300px)'),
-      Schema.const('thumbnail').description('缩略图'),
-    ])
-      .description('优先使用图片的最大尺寸。')
-      .default('large'),
-    autoResize: Schema.computed(Schema.boolean())
-      .experimental()
-      .default(false)
-      .description('根据 preferSize 自动缩小过大的图片。<br/> - 需要安装提供 canvas 服务的插件'),
-    asset: Schema.boolean().default(false).description('优先使用 [assets服务](https://assets.koishi.chat/) 转存图片。'),
-    base64: Schema.boolean().default(false).description('使用 base64 发送图片。'),
-    spoiler: Schema.union([
-      Schema.const(0).description('禁用'),
-      Schema.const(1).description('所有图片'),
-      Schema.const(2).description('仅 NSFW 图片'),
-    ])
-      .description('发送为隐藏图片，单击后显示（在 QQ 平台中以「合并转发」发送）。')
+      Schema.const('original'),
+      Schema.const('large'),
+      Schema.const('medium'),
+      Schema.const('small'),
+      Schema.const('thumbnail'),
+    ]).default('large'),
+    autoResize: Schema.computed(Schema.boolean()).experimental().default(false),
+    asset: Schema.boolean().default(false),
+    base64: Schema.boolean().default(false),
+    spoiler: Schema.union([Schema.const(0), Schema.const(1), Schema.const(2)])
       .default(0)
       .experimental(),
-    showTips: Schema.boolean().default(true).description('是否输出使用提示信息。'),
-  }).description('输出设置'),
-])
+    showTips: Schema.boolean().default(true),
+  }),
+]).i18n({
+  'zh-CN': require('./locales/zh-CN.schema'),
+})
 
 export function apply(ctx: Context, config: Config) {
   // @ts-expect-error inject structure not compatible
